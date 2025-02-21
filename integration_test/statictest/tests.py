555bd770c8a5221d2bd5beb37aa5fe60ef4dcd88
Stegopoelkus <115708462+Stegopoelkus@users.noreply.github.com>
2025-02-21 04:23:38 +0100

    fix compressed file gets deleted if origin file shrinks below MIN_SIZE (#211)
    Co-authored-by: Tim Eric Stache <tim_eric.stache@tu-dresden.de>

--- integration_test/statictest/tests.py
+++ integration_test/statictest/tests.py
@@ -36,14 +36,21 @@ class CollectStaticTest(SimpleTestCase):
             self.assertFileNotExist(self.temp_dir_path / file)
 
     def assertManifestStaticFiles(self):
-        for file in ["milligram.965a961f3962.css", "system.0177d7f30ce9.js", "speaker.5a6001289b0f.svg"]:
+        for file in [
+            "milligram.e824f9df03b0.css",
+            "system.0177d7f30ce9.js",
+            "speaker.5a6001289b0f.svg",
+        ]:
             self.assertFileExist(self.temp_dir_path / file)
             self.assertFileExist(self.temp_dir_path / (file + ".gz"))
             self.assertFileExist(self.temp_dir_path / (file + ".br"))
 
         self.assertFileExist(self.temp_dir_path / "not_compressed.9517ee88fcaa.txt")
 
-        for file in ["not_compressed.9517ee88fcaa.txt.gz", "not_compressed.9517ee88fcaa.txt.br"]:
+        for file in [
+            "not_compressed.9517ee88fcaa.txt.gz",
+            "not_compressed.9517ee88fcaa.txt.br",
+        ]:
             self.assertFileNotExist(self.temp_dir_path / file)
 
         for file in ("too_small.1fad53895b9b.js.gz", "too_small.1fad53895b9b.js.br"):
@@ -51,7 +58,7 @@ class CollectStaticTest(SimpleTestCase):
 
     def test_collectstatic_static(self):
         with self.settings(
-            STATICFILES_STORAGE="static_compress.storage.CompressedStaticFilesStorage",
+            STORAGES={"staticfiles": {"BACKEND": "static_compress.storage.CompressedStaticFilesStorage"}},
             STATIC_COMPRESS_MIN_SIZE_KB=1,
             STATIC_ROOT=self.temp_dir.name,
         ):
@@ -61,13 +68,19 @@ class CollectStaticTest(SimpleTestCase):
 
     def test_collectstatic_manifest(self):
         with self.settings(
-            STATICFILES_STORAGE="static_compress.storage.CompressedManifestStaticFilesStorage",
+            STORAGES={"staticfiles": {"BACKEND": "static_compress.storage.CompressedManifestStaticFilesStorage"}},
             STATIC_COMPRESS_MIN_SIZE_KB=1,
             STATIC_ROOT=self.temp_dir.name,
         ):
             call_command("collectstatic", interactive=False, verbosity=0)
 
-            for file in ["milligram.css", "system.js", "not_compressed.txt", "speaker.svg", "staticfiles.json"]:
+            for file in [
+                "milligram.css",
+                "system.js",
+                "not_compressed.txt",
+                "speaker.svg",
+                "staticfiles.json",
+            ]:
                 self.assertFileExist(self.temp_dir_path / file)
                 self.assertFileNotExist(self.temp_dir_path / (file + ".gz"))
                 self.assertFileNotExist(self.temp_dir_path / (file + ".br"))
@@ -76,7 +89,7 @@ class CollectStaticTest(SimpleTestCase):
 
     def test_collectstatic_only_gz(self):
         with self.settings(
-            STATICFILES_STORAGE="static_compress.storage.CompressedStaticFilesStorage",
+            STORAGES={"staticfiles": {"BACKEND": "static_compress.storage.CompressedStaticFilesStorage"}},
             STATIC_COMPRESS_MIN_SIZE_KB=1,
             STATIC_COMPRESS_METHODS=["gz"],
             STATIC_ROOT=self.temp_dir.name,
@@ -92,7 +105,7 @@ class CollectStaticTest(SimpleTestCase):
 
     def test_collectstatic_changed_file_ext(self):
         with self.settings(
-            STATICFILES_STORAGE="static_compress.storage.CompressedStaticFilesStorage",
+            STORAGES={"staticfiles": {"BACKEND": "static_compress.storage.CompressedStaticFilesStorage"}},
             STATIC_COMPRESS_MIN_SIZE_KB=1,
             STATIC_COMPRESS_FILE_EXTS=("js", "css"),
             STATIC_ROOT=self.temp_dir.name,
@@ -108,7 +121,7 @@ class CollectStaticTest(SimpleTestCase):
 
     def test_collectstatic_empty_file_ext(self):
         with self.settings(
-            STATICFILES_STORAGE="static_compress.storage.CompressedStaticFilesStorage",
+            STORAGES={"staticfiles": {"BACKEND": "static_compress.storage.CompressedStaticFilesStorage"}},
             STATIC_COMPRESS_MIN_SIZE_KB=1,
             STATIC_COMPRESS_FILE_EXTS=[],
             STATIC_ROOT=self.temp_dir.name,
@@ -121,7 +134,7 @@ class CollectStaticTest(SimpleTestCase):
 
     def test_collectstatic_not_keep_original(self):
         with self.settings(
-            STATICFILES_STORAGE="static_compress.storage.CompressedStaticFilesStorage",
+            STORAGES={"staticfiles": {"BACKEND": "static_compress.storage.CompressedStaticFilesStorage"}},
             STATIC_COMPRESS_MIN_SIZE_KB=1,
             STATIC_COMPRESS_KEEP_ORIGINAL=False,
             STATIC_ROOT=self.temp_dir.name,
@@ -134,7 +147,7 @@ class CollectStaticTest(SimpleTestCase):
 
     def test_collectstatic_with_zlib(self):
         with self.settings(
-            STATICFILES_STORAGE="static_compress.storage.CompressedStaticFilesStorage",
+            STORAGES={"staticfiles": {"BACKEND": "static_compress.storage.CompressedStaticFilesStorage"}},
             STATIC_COMPRESS_MIN_SIZE_KB=1,
             STATIC_COMPRESS_METHODS=["gz+zlib", "br"],
             STATIC_ROOT=self.temp_dir.name,
@@ -146,7 +159,7 @@ class CollectStaticTest(SimpleTestCase):
     def test_collectstatic_twice_replace(self):
         with tempfile.TemporaryDirectory() as static_dir:
             with self.settings(
-                STATICFILES_STORAGE="static_compress.storage.CompressedStaticFilesStorage",
+                STORAGES={"staticfiles": {"BACKEND": "static_compress.storage.CompressedStaticFilesStorage"}},
                 STATIC_COMPRESS_MIN_SIZE_KB=1,
                 STATIC_COMPRESS_METHODS=["gz+zlib"],
                 STATIC_ROOT=self.temp_dir.name,
@@ -178,3 +191,37 @@ class CollectStaticTest(SimpleTestCase):
 
                 with compressed_file_path.open("rb") as fp:
                     self.assertEqual(gzip.open(fp).read(), expected_content)
+
+    def test_collectstatic_delete_shrunken_files(self):
+        with tempfile.TemporaryDirectory() as static_dir:
+            with self.settings(
+                STORAGES={"staticfiles": {"BACKEND": "static_compress.storage.CompressedStaticFilesStorage"}},
+                STATIC_COMPRESS_MIN_SIZE_KB=1,
+                STATIC_ROOT=self.temp_dir.name,
+                STATICFILES_DIRS=[static_dir],
+            ):
+                output_file_path = self.temp_dir_path / "test.js"
+                compressed_file_path = self.temp_dir_path / "test.js.gz"
+
+                static_file = Path(static_dir) / "test.js"
+                with static_file.open("wb") as fp:
+                    fp.write(b"a" * 5000)
+
+                call_command("collectstatic", interactive=False, verbosity=0)
+
+                self.assertFileExist(output_file_path)
+                self.assertFileExist(compressed_file_path)
+
+                # Fake that the file has been written long ago
+                os.utime(output_file_path, times=(1, 1))
+                os.utime(compressed_file_path, times=(1, 1))
+
+                expected_content = b"b" * 1023
+                with static_file.open("wb") as fp:
+                    fp.write(expected_content)
+
+                call_command("collectstatic", interactive=False, verbosity=0)
+
+                self.assertFileExist(output_file_path)
+                self.assertEqual(output_file_path.read_bytes(), expected_content)
+                self.assertFileNotExist(compressed_file_path)
